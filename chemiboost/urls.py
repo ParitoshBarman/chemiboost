79d9d4e218b4807ca3b79683799e2340ab30d229
ParitoshBarman <barmanpari163@gmail.com>
2025-01-13 21:22:11 +0530

    successfully created superuser

--- chemiboost/urls.py
+++ chemiboost/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+admin.site.site_header = "Chemi Boost Admin"
+admin.site.site_title = "Chemi Boost Admin Portal"
+admin.site.index_title = "Chemi Boost Portal"
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('chemiboostapp.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+
---
+++ chemiboostapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ChemiboostappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'chemiboostapp'
